8de52341fdffa67b8fb9133ce14a222329002688
Talh Rahim <talhagujjar5199@gmail.com>
2026-02-24 11:08:25 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+
+export default function Home() {
+  return (
+    <main>
+      <img src="/slides/slide1.jpeg" alt="Slide 1" className="slide"/>
+<img src="/slides/slide2.jpeg" alt="Slide 2" className="slide"/>
+<img src="/slides/slide3.jpeg" alt="Slide 3" className="slide"/>
+<img src="/slides/slide4.jpeg" alt="Slide 4" className="slide"/>
+<img src="/slides/slide5.jpeg" alt="Slide 5" className="slide"/>
+<img src="/slides/slide6.jpeg" alt="Slide 6" className="slide"/>
+<img src="/slides/slide7.jpeg" alt="Slide 7" className="slide"/>
+<img src="/slides/slide8.jpeg" alt="Slide 8" className="slide"/>
+<img src="/slides/slide9.jpeg" alt="Slide 9" className="slide"/>
+<img src="/slides/slide10.jpeg" alt="Slide 10" className="slide"/>
+<img src="/slides/slide11.jpeg" alt="Slide 11" className="slide"/>
+<img src="/slides/slide12.jpeg" alt="Slide 12" className="slide"/>
+    </main>
+  );
+}
