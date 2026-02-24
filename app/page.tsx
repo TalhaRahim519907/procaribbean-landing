c2b0ab179bf0bb75a785c37ec26f1306bc26949e
Talh Rahim <talhagujjar5199@gmail.com>
2026-02-24 14:11:57 +0500

    final-changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@
 export default function Home() {
   return (
     <main>
-      <img src="/slides/slide1.jpeg" alt="Slide 1" className="slide"/>
-<img src="/slides/slide2.jpeg" alt="Slide 2" className="slide"/>
-<img src="/slides/slide3.jpeg" alt="Slide 3" className="slide"/>
-<img src="/slides/slide4.jpeg" alt="Slide 4" className="slide"/>
-<img src="/slides/slide5.jpeg" alt="Slide 5" className="slide"/>
-<img src="/slides/slide6.jpeg" alt="Slide 6" className="slide"/>
-<img src="/slides/slide7.jpeg" alt="Slide 7" className="slide"/>
-<img src="/slides/slide8.jpeg" alt="Slide 8" className="slide"/>
-<img src="/slides/slide9.jpeg" alt="Slide 9" className="slide"/>
-<img src="/slides/slide10.jpeg" alt="Slide 10" className="slide"/>
-<img src="/slides/slide11.jpeg" alt="Slide 11" className="slide"/>
-<img src="/slides/slide12.jpeg" alt="Slide 12" className="slide"/>
+      <img src="/slides/slide1.png" alt="Slide 1" className="slide"/>
+<img src="/slides/slide2.png" alt="Slide 2" className="slide"/>
+<img src="/slides/slide3.png" alt="Slide 3" className="slide"/>
+<img src="/slides/slide4.png" alt="Slide 4" className="slide"/>
+<img src="/slides/slide5.png" alt="Slide 5" className="slide"/>
+<img src="/slides/slide6.png" alt="Slide 6" className="slide"/>
+<img src="/slides/slide7.png" alt="Slide 7" className="slide"/>
+<img src="/slides/slide8.png" alt="Slide 8" className="slide"/>
+<img src="/slides/slide9.png" alt="Slide 9" className="slide"/>
+<img src="/slides/slide10.png" alt="Slide 10" className="slide"/>
+<img src="/slides/slide11.png" alt="Slide 11" className="slide"/>
+<img src="/slides/slide12.png" alt="Slide 12" className="slide"/>
     </main>
   );
 }
